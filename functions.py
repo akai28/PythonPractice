e5e32874ed16b7031973319073b3af98bbfc9d52
akai28 <akanksha.tiwari2801@gmail.com>
2021-01-31 22:27:55 +0530

    Python Functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+# Create function
+# def sayHello(name="Ethan Hunt"):
+#     print(f"Hello {name}")
+
+
+# sayHello()
+
+# Return values
+
+
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+num = getSum(10, 30)
+print(num)
