f067536a1a7f29cb772bf5ea99aed19eb0c60f67
akai28 <akanksha.tiwari2801@gmail.com>
2021-02-01 11:07:53 +0530

    Python Tuples

---
+++ tuples_set.py
@@ -0,0 +1,43 @@
+# Create tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+
+# Using a constructor
+fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+# Single value needs trailing comma
+fruits2 = ('Apples',)
+
+# Get value
+print(fruits[1])
+
+# Can't change value
+fruits[0] = 'Pears'
+
+# Delete tuple
+del fruits2
+
+# Get length
+print(len(fruits))
+
+# Create set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grape')
+
+# Remove from set
+fruits_set.remove('Grape')
+
+# Add duplicate
+fruits_set.add('Apples')
+
+# Clear set
+fruits_set.clear()
+
+# Delete
+del fruits_set
+
+print(fruits_set)
