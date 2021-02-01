e4e698d4734af28536f376af933c0efa21f5d56d
akai28 <akanksha.tiwari2801@gmail.com>
2021-02-01 11:51:14 +0530

    Python classes

---
+++ classes.py
@@ -0,0 +1,65 @@
+# Create class
+class User:
+
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+        self._private = 1000
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def has_birthday(self):
+        self.age += 1
+
+   # function for encap variable
+    def print_encap(self):
+        print(self._private)
+
+# Extend class
+
+
+class Customer(User):
+    # Constructor
+    def __init__(self, name, email, age):
+        # Called proper parent class constructor to make this as proper child inehriting all methods.
+        User.__init__(self, name, email, age)
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and my balance is {self.balance}'
+
+
+#  Init user object
+akai = User('Akanksha Tiwari', 'ak_tiwari@gmail.com', 19)
+# Init customer object
+utk = Customer('Utkarsh Tiwari', 'utk@yahoo.com', 25)
+
+utk.set_balance(500)
+print(utk.greeting())
+
+akai.has_birthday()
+print(akai.greeting())
+
+# Encapsulation -->
+akai.print_encap()
+akai._private = 800
+akai.print_encap()
+
+# Method inherited from parent
+# Changing the variable for brad doesn't affect janets variable --> Encapsulation
+utk.print_encap()
+utk._private = 600
+utk.print_encap()
+
+# Similary changing janet's doesn't affect brad's variable.
+akai.print_encap()
